60045ea80369f22374cb04b7d0741ed6005cc784
Dikshita621 <dikshitachoudhary845@gmail.com>
2025-09-15 21:37:44 +0530

    Create Que52.c

---
+++ Que52.c
@@ -0,0 +1,29 @@
+/*Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+*/
+#include <stdio.h>
+
+int main() {
+    int n = 14;
+    for (int i = 0; i < n; i++) {
+        printf("*\n");
+    }
+    return 0;
+}
